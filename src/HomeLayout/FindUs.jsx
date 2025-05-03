9b0736ba90a97cc31039fdcc41f4e4e61c417b47
maysha29 <mayshatabassum29@gmail.com>
2025-05-04 00:53:45 +0600

    QZone part added

--- src/HomeLayout/FindUs.jsx
+++ src/HomeLayout/FindUs.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { FaFacebook } from "react-icons/fa6";
+import { AiFillTwitterCircle } from "react-icons/ai";
+import { FaInstagram } from "react-icons/fa";
 
 const FindUs = () => {
     return (
         <div>
             <h2 className='font-bold mb-5'>Find Us on</h2>
+            <div className="join join-vertical w-full">
+             <button className="btn bg-base-100 justify-start join-item"><FaFacebook />Facebook</button>
+             <button className="btn bg-base-100 justify-start join-item"><AiFillTwitterCircle />Twitter</button>
+             <button className="btn bg-base-100 justify-start join-item"><FaInstagram />Instagram</button>
+            </div>
         </div>
     );
 };
---
+++ src/HomeLayout/QZone.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const QZone = () => {
+    return (
+        <div className='bg-base-200 p-3'>
+            <h2 className='font-bold mb-5'>QZone</h2>
+        </div>
+    );
+};
+
+export default QZone;
