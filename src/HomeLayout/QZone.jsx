5bab488e6b626364cef3ff2f3070c8568d248d21
maysha29 <mayshatabassum29@gmail.com>
2025-05-04 11:45:31 +0600

    Login page added

--- src/HomeLayout/QZone.jsx
+++ src/HomeLayout/QZone.jsx
@@ -4,6 +4,12 @@ const QZone = () => {
     return (
         <div className='bg-base-200 p-3'>
             <h2 className='font-bold mb-5'>QZone</h2>
+            <div>
+                <img src="/swimming.png" alt="" />
+                <img src="/class.png" alt="" />
+                <img src="/playground.png" alt="" />
+            </div>
+
         </div>
     );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
+import { Navigate } from 'react-router';
 
 const Home = () => {
     return (
-        <div><h2>home</h2></div>
-        
+      <Navigate to='/category/1'></Navigate>
     );
 };
 
